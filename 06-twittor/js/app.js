3276b3e448e092f26d86b357b6fbe433582c76c8
Emiliano <emilianoruben91@gmail.com>
2020-04-21 15:33:33 -0500

    Se agrego un ejemplo para convertir unapagina web en pwa

--- 06-twittor/js/app.js
+++ 06-twittor/js/app.js
@@ -1,3 +1,12 @@
+var url=window.location.href;
+var swLocation='/twittor/sw.js';
+
+if (navigator.serviceWorker) {
+    if (url.includes('localhost')) {
+        swLocation='/sw.js';
+    }
+    navigator.serviceWorker.register(swLocation);
+}
 
 // Referencias de jQuery
 
